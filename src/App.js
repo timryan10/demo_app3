af0d59504395e3d21e188f20aa9e1292a7cbbe6c
Tim <rtim10@ymail.com>
2023-11-17 22:46:33 -0500

    initial commit: learning react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [counter,setCounter] = useState(0)
+
+  const increment = () => {
+    setCounter(currVal => {
+      return currVal + 1
+    })
+  }
+  const decrease = () => {
+    if (counter === 0) return
+    setCounter(currVal => {
+      return currVal - 1
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <p>counter: {counter}</p>
+    <button onClick={decrease}>-</button>
+    <button onClick={increment}>+</button>
+   </div>
   );
 }
 
